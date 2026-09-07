29fc8063fd664d284f6dde1a4399045560b36a6b
Daniel Harrington <danielharrington06@outlook.com>
2026-09-07 16:38:43 +0100

    2C: moved heuristic above findRoute so it can be used

--- src/router.cpp
+++ src/router.cpp
@@ -17,6 +17,16 @@ struct CompareQueueNode { // needed to tell the queue to prioritise the least f 
     }
 };
 
+double heuristic(const RoadNetwork& network, uint32_t current, uint32_t destination) { // for test network, using latitude and longitiude as cartesian coordinates, but on real data, we will need geographic distance calculation
+    const RoadNode& currentNode = network.getNode(current);
+    const RoadNode& destinationNode = network.getNode(destination);
+
+    double dx = currentNode.latitude - destinationNode.latitude;
+    double dy = currentNode.longitude - destinationNode.longitude;
+
+    return std::sqrt(dx*dx + dy*dy);
+}
+
 Route findRoute(const RoadNetwork& network, uint32_t start, uint32_t destination) {
     Route route;
 
@@ -116,14 +126,4 @@ Route findRoute(const RoadNetwork& network, uint32_t start, uint32_t destination
     }
 
     return route;
-}
-
-double heuristic(const RoadNetwork& network, uint32_t current, uint32_t destination) { // for test network, using latitude and longitiude as cartesian coordinates, but on real data, we will need geographic distance calculation
-    const RoadNode& currentNode = network.getNode(current);
-    const RoadNode& destinationNode = network.getNode(destination);
-
-    double dx = currentNode.latitude - destinationNode.latitude;
-    double dy = currentNode.longitude - destinationNode.longitude;
-
-    return std::sqrt(dx*dx + dy*dy);
 }
