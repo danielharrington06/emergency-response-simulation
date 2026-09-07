43707178e819ce2ddd106d71681dc885aba88f4c
Daniel Harrington <danielharrington06@outlook.com>
2026-09-07 15:07:42 +0100

    2B: test network that will be used added with a slightly more normal and less regular road structure

--- src/test_network.cpp
+++ src/test_network.cpp
@@ -4,22 +4,115 @@ RoadNetwork createTestNetwork() {
 
     RoadNetwork network;
 
-    uint32_t node0 = network.addNode(1000, 0.0, 1.0);
-    uint32_t node1 = network.addNode(1001, 1.0, 2.0);
-    uint32_t node2 = network.addNode(1002, 2.0, 2.0);
-    uint32_t node3 = network.addNode(1003, 3.0, 1.0);
-    uint32_t node4 = network.addNode(1004, 1.0, 0.0);
-    uint32_t node5 = network.addNode(1005, 2.0, 0.0);
+    // Nodes
+    uint32_t node0 = network.addNode(1000, 0.0, 2.0);
+    uint32_t node1 = network.addNode(1001, 1.5, 4.0);
+    uint32_t node2 = network.addNode(1002, 4.0, 4.2);
+    uint32_t node3 = network.addNode(1003, 5.5, 2.0);
+    uint32_t node4 = network.addNode(1004, 1.7, 0.2);
+    uint32_t node5 = network.addNode(1005, 4.2, 0.0);
 
+    // 0 <-> 1
     network.addEdge(node0, RoadEdge{
         .destination = node1,
-        .distance = 1.0,
+        .distance = 2.7,
         .speedLimit = 30
     });
 
     network.addEdge(node1, RoadEdge{
         .destination = node0,
-        .distance = 1.0,
+        .distance = 2.7,
+        .speedLimit = 30
+    });
+
+    // 1 <-> 2
+    network.addEdge(node1, RoadEdge{
+        .destination = node2,
+        .distance = 2.5,
+        .speedLimit = 40
+    });
+
+    network.addEdge(node2, RoadEdge{
+        .destination = node1,
+        .distance = 2.5,
+        .speedLimit = 40
+    });
+
+    // 2 <-> 3
+    network.addEdge(node2, RoadEdge{
+        .destination = node3,
+        .distance = 2.8,
+        .speedLimit = 30
+    });
+
+    network.addEdge(node3, RoadEdge{
+        .destination = node2,
+        .distance = 2.8,
+        .speedLimit = 30
+    });
+
+    // 0 <-> 4
+    network.addEdge(node0, RoadEdge{
+        .destination = node4,
+        .distance = 2.6,
+        .speedLimit = 30
+    });
+
+    network.addEdge(node4, RoadEdge{
+        .destination = node0,
+        .distance = 2.6,
+        .speedLimit = 30
+    });
+
+    // 4 <-> 5
+    network.addEdge(node4, RoadEdge{
+        .destination = node5,
+        .distance = 2.5,
+        .speedLimit = 30
+    });
+
+    network.addEdge(node5, RoadEdge{
+        .destination = node4,
+        .distance = 2.5,
+        .speedLimit = 30
+    });
+
+    // 5 <-> 3
+    network.addEdge(node5, RoadEdge{
+        .destination = node3,
+        .distance = 2.6,
+        .speedLimit = 40
+    });
+
+    network.addEdge(node3, RoadEdge{
+        .destination = node5,
+        .distance = 2.6,
+        .speedLimit = 40
+    });
+
+    // 1 <-> 4
+    network.addEdge(node1, RoadEdge{
+        .destination = node4,
+        .distance = 3.8,
+        .speedLimit = 30
+    });
+
+    network.addEdge(node4, RoadEdge{
+        .destination = node1,
+        .distance = 3.8,
+        .speedLimit = 30
+    });
+
+    // 2 <-> 5
+    network.addEdge(node2, RoadEdge{
+        .destination = node5,
+        .distance = 4.2,
+        .speedLimit = 30
+    });
+
+    network.addEdge(node5, RoadEdge{
+        .destination = node2,
+        .distance = 4.2,
         .speedLimit = 30
     });
 
